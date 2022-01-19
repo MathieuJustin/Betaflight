50f7c6a646ab72a48dd41eec5f65fc5005981f72
MathieuJustin <97806107+MathieuJustin@users.noreply.github.com>
2022-01-19 15:54:05 -0500

    target.mk modifications

--- src/main/target/DB8548/target.mk
+++ src/main/target/DB8548/target.mk
@@ -1,12 +1,9 @@
-F3_TARGETS  += $(TARGET)
+F427II_TARGETS += $(TARGET)
 
-FEATURES    = VCP ONBOARDFLASH
+HSE_VALUE   = 25000000
 
-FEATURE_CUT_LEVEL = 1
+FEATURES    += VCP ONBOARDFLASH
 
 TARGET_SRC = \
-            drivers/accgyro/accgyro_mpu.c \
-            drivers/accgyro/accgyro_spi_mpu6000.c \
-            drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6500.c \
-            drivers/barometer/barometer_bmp280.c
+            drivers/accgyro/accgyro_mpu6500.c
